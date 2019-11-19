055036705b40bea55e5456a4d4c13c3ed4d8f7f1
ZAP <mreza034@gmail.com>
2019-11-19 14:32:01 +0700

    Initial commit

---
+++ Course.java
@@ -0,0 +1,11 @@
+
+public class Course {
+	
+	public void getCourse(String fileInput) {
+		
+	}
+	
+	public void getConflictMatrix(String fileInput) {
+		
+	}
+}
---
+++ Schedule.java
@@ -0,0 +1,13 @@
+
+public class Schedule {
+	
+	public void scheduling() {
+		
+	}
+	public void schedulingByDegree() {
+		
+	}
+	public void printSchedule() {
+		
+	}
+}
